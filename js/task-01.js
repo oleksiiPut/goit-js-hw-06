256d8fe2c719143532877d3f0cce098f3702e67e
Oleksii Putnenko <oleksii.putnenko@gmail.com>
2022-05-25 10:37:31 +0300

    fix

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,12 @@
 const categoryRef = document.querySelectorAll(".item");
 console.log("Number of categories: ", categoryRef.length);
 
-for (const item of categoryRef) {
-    console.log("Category: ", item.firstElementChild.textContent);
-    console.log("Elements: ", item.lastElementChild.children.length);
-}
+// for (const item of categoryRef) {
+//     console.log("Category: ", item.firstElementChild.textContent);
+//     console.log("Elements: ", item.lastElementChild.children.length);
+// }
+
+categoryRef.forEach((element) => {
+  console.log(`Category: ${element.firstElementChild.textContent}`);
+  console.log(`Elements: ${element.lastElementChild.children.length}`);
+});
