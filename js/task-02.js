bf572cd667ae0564c8285ee3fec6150196c57c3e
Oleksii Putnenko <oleksii.putnenko@gmail.com>
2022-05-26 12:50:20 +0300

    fix2

--- js/task-02.js
+++ js/task-02.js
@@ -9,36 +9,11 @@ const ingredients = [
 
 const ingredientsList = document.querySelector("#ingredients");
 
-// const elem1 = document.createElement("li");
-// elem1.textContent = "Potatoes";
-// elem1.classList.add("item");
-
-// const elem2 = document.createElement("li");
-// elem2.textContent = "Mushrooms";
-// elem2.classList.add("item");
-
-// const elem3 = document.createElement("li");
-// elem3.textContent = "Garlic";
-// elem3.classList.add("item");
-
-// const elem4 = document.createElement("li");
-// elem4.textContent = "Tomatos";
-// elem4.classList.add("item");
-
-// const elem5 = document.createElement("li");
-// elem5.textContent = "Herbs";
-// elem5.classList.add("item");
-
-// const elem6 = document.createElement("li");
-// elem6.textContent = "Condiments";
-// elem6.classList.add("item");
-
-// ingredientsList.append(elem1, elem2, elem3, elem4, elem5, elem6);
-// console.log(ingredientsList);
-
-const recipeIngredients = ingredients.forEach((ingredient) => {
+const recipeIngredients = ingredients.map((ingredient) => {
   const ingredientsItem = document.createElement("li");
   ingredientsItem.textContent = ingredient;
   ingredientsItem.classList.add("item");
-  ingredientsList.append(ingredientsItem);
+  return ingredientsItem;
 });
+
+ingredientsList.append(...recipeIngredients);
